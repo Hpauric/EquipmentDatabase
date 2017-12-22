b6f185c32e46bd358e411e4a74d011c5be074832
Pauric <pauric.holleran@gmail.com>
2017-12-22 12:24:49 +0000

    foreign key cascade change for equipment table

--- EquipmentDatabase/Controllers/StudentController.cs
+++ EquipmentDatabase/Controllers/StudentController.cs
@@ -106,11 +106,16 @@ namespace EquipmentDatabase.Controllers
         }
 
         // GET: Student/Delete/5
-        public ActionResult Delete(int? id)
+     
+        public ActionResult Delete(int? id, bool? saveChangesError = false)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            if (saveChangesError.GetValueOrDefault())
+            {
+                ViewBag.ErrorMessage = "Delete failed. Try again, and if the problem persists see your system administrator.";
             }
             Student student = db.Students.Find(id);
             if (student == null)
@@ -121,13 +126,21 @@ namespace EquipmentDatabase.Controllers
         }
 
         // POST: Student/Delete/5
-        [HttpPost, ActionName("Delete")]
+        [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
+        public ActionResult Delete(int id)
         {
-            Student student = db.Students.Find(id);
-            db.Students.Remove(student);
-            db.SaveChanges();
+            try
+            {
+                Student student = db.Students.Find(id);
+                db.Students.Remove(student);
+                db.SaveChanges();
+            }
+            catch (DataException/* dex */)
+            {
+                //Log the error (uncomment dex variable name and add a line here to write a log.
+                return RedirectToAction("Delete", new { id = id, saveChangesError = true });
+            }
             return RedirectToAction("Index");
         }
 
